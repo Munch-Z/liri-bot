712dbb295e476a2435ce2effec1e8307fb7bf97e
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-07-26 16:06:09 -0700

    OMDB Done

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const Spotify = require('node-spotify-api');
 const keys = require("./keys.js");
+const axios = require('axios');
+const moment = require('moment');
 const args = process.argv.slice(2);
 
 const op = args[0];
@@ -9,25 +11,47 @@ const userInput = args[1];
 
 
 
-function searchSpotify (query) {
+function searchSpotify(query) {
     let spotify = new Spotify(keys.spotify);
 
-    spotify.search({ type: 'track', query: query, limit: 1 }, function(err, data) {
+    spotify.search({ type: 'track', query: query, limit: 1 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-    
+
         let importantInfo = data.tracks.items[0];
         console.log(importantInfo.artists[0].name); //Artist
         console.log(importantInfo.album.name); //Album
         console.log(importantInfo.preview_url); //Preview
         console.log(importantInfo.name);  //Name of song
-      });  
+    });
 }
 
-switch(op) {
+function searchOMDB(query) {
+
+    axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=eaff8485&t=${query}`).then((data) => {
+
+        let movie = data.data;
+        console.log(movie.Title); //title
+        console.log(movie.Year); //year
+        console.log(movie.imdbRating); //IMDB rating
+        console.log(movie.Ratings[1].Value); //Rotten Tomatoes
+        console.log(movie.Country); //Country where produced
+        console.log(movie.Language); //Language
+        console.log(movie.Plot); //Plot
+        console.log(movie.Actors); //Actors
+
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+switch (op) {
     case 'spotify-this-song':
         searchSpotify(userInput);
+        break;
+    case 'movie-this':
+        searchOMDB(userInput);
         break;
     default:
         console.log('INVALID INPUT');
